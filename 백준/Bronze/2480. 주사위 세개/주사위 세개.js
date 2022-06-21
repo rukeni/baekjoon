3b8e3be65f67d7583717e04553ffa3f628fe3fc0
양탕구리 <rukeni@naver.com>
2022-06-21 15:08:50 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number);
+
+const uniqueNumbersSize = (numbers) => {
+  return new Set(numbers).size;
+}
+
+const getSameNumber = (numbers) => {
+  if(numbers[0] === numbers[1]) {
+    return numbers[0];
+  }
+  if(numbers[0] === numbers[2]) {
+    return numbers[0];
+  }
+  return numbers[1];
+}
+
+function solution(numbers) {
+  const reward = {
+    1: 10000 + numbers[0] * 1000,
+    2: 1000 + getSameNumber(numbers) * 100,
+    3: 0 + Math.max(...numbers) * 100,
+  };
+
+  const uniqueSize = uniqueNumbersSize(numbers);
+
+  return reward[uniqueSize];
+}
+
+console.log(solution(input));
