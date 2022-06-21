a94bf5889ed3a52a2d250b8486a8741290065ee2
양탕구리 <rukeni@naver.com>
2022-06-21 14:06:12 +0900

    [Bronze III] Title: 알람 시계, Time: 200 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -0,0 +1,25 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number);
+
+const [hour, minute] = input;
+
+const convertToMinute = (hour, minute) => (hour * 60 + minute) - 45;
+
+const minuteToTime = (minute) => {
+  const hour = Math.floor(minute / 60);
+  const minute_ = minute % 60;
+
+  return `${hour} ${minute_}`;
+}
+
+function solution(hour, minute) {
+  const minute_ = convertToMinute(hour, minute);
+
+  if(minute_ >= 0) {
+    return minuteToTime(minute_);
+  }
+
+  return minuteToTime(minute_ + 1440);
+}
+
+console.log(solution(hour, minute));
