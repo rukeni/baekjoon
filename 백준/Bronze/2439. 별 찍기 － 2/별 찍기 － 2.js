5f1b02c7e6205c1581af433425fbca972aa3f380
양탕구리 <rukeni@naver.com>
2022-06-26 16:58:10 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 120 ms, Memory: 9512 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+const count = Number(input);
+
+let answer = '';
+
+for(let i = 1; i <= count; i++) {
+  const emptySpace = new Array(count - i).fill(' ').join('');
+  const star = new Array(i).fill('*').join('');
+    
+  answer += (emptySpace + star + '\n');
+}
+
+console.log(answer);
