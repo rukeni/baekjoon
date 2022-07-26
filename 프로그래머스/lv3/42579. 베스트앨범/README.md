a51a699889f888fc89f6b414effe6ade9d8a2b0b
양탕구리 <rukeni@naver.com>
2022-07-26 20:17:03 +0900

    [level 3] Title: 베스트앨범, Time: 0.17 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/README.md
+++ 프로그래머스/lv3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=javascript) 
 
 ### 성능 요약
 
-메모리: 30.1 MB, 시간: 0.54 ms
+메모리: 30 MB, 시간: 0.17 ms
 
 ### 구분
 
--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -12,15 +12,18 @@ function solution(genres, plays) {
     
     const genreKeys = Object.keys(genreTable);
     
+    // 장르별로 플레이가 많이 된 순서대로 정렬하고 배열 내용물을 2개만 남긴다.
     for (genre of genreKeys) {
         genreTable[genre].sort((a, b) => b[0] - a[0]);
         genreTable[genre] = genreTable[genre].slice(0,2);
     }
     
+    // 플레이 총합이 더 많은 장르순으로 정렬한다.
     genreKeys.sort((a, b) => totalTable[b] - totalTable[a]);
     
     let answer = [];
     
+    // 가지고 있던 배열을 모두 더하면서 정답처럼 index만 남겨둔다.
     for (genre of genreKeys) {
         answer = [...answer, ...genreTable[genre].map(gen => gen[1])];
     }
